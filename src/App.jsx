c9f69a038d18b1563977754ccddc325c6be71945
jrajan20 <jai_20j@hotmail.com>
2026-07-07 01:02:04 -0400

    used copilot to build routes structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Account from './pages/Account'
+import Bookings from './pages/Bookings'
+import Cabins from './pages/Cabins'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import PageNotFound from './pages/PageNotFound'
+import Settings from './pages/Settings'
+import Users from './pages/Users'
+
 function App() {
   return (
-    <div>
-      Hello Oasis!
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigate replace to="/dashboard" />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/bookings" element={<Bookings />} />
+      <Route path="/cabins" element={<Cabins />} />
+      <Route path="/users" element={<Users />} />
+      <Route path="/settings" element={<Settings />} />
+      <Route path="/account" element={<Account />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
   )
 }
 
