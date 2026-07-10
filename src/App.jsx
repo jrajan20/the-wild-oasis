2a5f84ccf058925f4b6155debb0a397c618ee6af
jrajan20 <jai_20j@hotmail.com>
2026-07-09 21:04:50 -0400

    adding styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 
 import { Navigate, Route, Routes } from 'react-router-dom'
+
+import styled from 'styled-components'
 import Account from './pages/Account'
 import Bookings from './pages/Bookings'
 import Cabins from './pages/Cabins'
@@ -9,19 +11,28 @@ import PageNotFound from './pages/PageNotFound'
 import Settings from './pages/Settings'
 import Users from './pages/Users'
 
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  color: #333;
+` 
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate replace to="/dashboard" />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/bookings" element={<Bookings />} />
-      <Route path="/cabins" element={<Cabins />} />
-      <Route path="/users" element={<Users />} />
-      <Route path="/settings" element={<Settings />} />
-      <Route path="/account" element={<Account />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="*" element={<PageNotFound />} />
-    </Routes>
+    <div>
+      <H1>Welcome to the Wild Oasis</H1>
+    </div>
+    //   <Routes>
+    //   <Route path="/" element={<Navigate replace to="/dashboard" />} />
+    //   <Route path="/dashboard" element={<Dashboard />} />
+    //   <Route path="/bookings" element={<Bookings />} />
+    //   <Route path="/cabins" element={<Cabins />} />
+    //   <Route path="/users" element={<Users />} />
+    //   <Route path="/settings" element={<Settings />} />
+    //   <Route path="/account" element={<Account />} />
+    //   <Route path="/login" element={<Login />} />
+    //   <Route path="*" element={<PageNotFound />} />
+    // </Routes>
   )
 }
 
