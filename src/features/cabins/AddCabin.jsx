2302b3a4740006dbd4637149a92d148facf783f7
jrajan20 <jai_20j@hotmail.com>
2026-07-13 11:08:17 -0400

    Used a compound component pattern  to make  table more reusable, and built a component called Table

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -9,12 +9,12 @@ function AddCabin() {
       <Modal.Open opens="create-cabin">
         <Button>Add new cabin</Button>
       </Modal.Open>
-      <Modal.Open opens="cabin-table">
+      {/* <Modal.Open opens="cabin-table">
         <Button>Show cabin table</Button>
       </Modal.Open>
       <Modal.Window name="cabin-table">
         <CabinTable />
-      </Modal.Window>
+      </Modal.Window> */}
       <Modal.Window name="create-cabin">
         <CreateCabinForm />
       </Modal.Window>
