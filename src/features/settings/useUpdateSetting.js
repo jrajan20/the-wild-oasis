6c832bfa91ea054f190769275e7c14ca28bd39c2
jrajan20 <jai_20j@hotmail.com>
2026-07-13 01:14:45 -0400

    added settings page and custom hooks for loading/updating settings.  Settings update succesful

---
+++ src/features/settings/useUpdateSetting.js
@@ -0,0 +1,20 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+import { updateSetting } from "../../services/apiSettings";
+
+export function useUpdateSetting() {
+  const queryClient = useQueryClient();
+
+  const { mutate: editSetting, isLoading: isUpdating } = useMutation({
+    mutationFn: updateSetting,
+    onSuccess: () => {
+      toast.success("Setting updated successfully");
+      queryClient.invalidateQueries({ queryKey: ["settings"] });
+    },
+    onError: (err) => {
+      toast.error(err.message || "Error updating setting");
+    },
+  });
+
+  return { editSetting, isUpdating };
+}
