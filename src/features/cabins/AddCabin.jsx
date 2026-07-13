1bca450017181bbd2c98d8dffb328a3e0b07e3f6
jrajan20 <jai_20j@hotmail.com>
2026-07-13 10:44:36 -0400

    created a show table modal window

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,6 +1,7 @@
 import Modal from "../../ui/Modal";
 import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
+import CabinTable from "./CabinTable";
 
 function AddCabin() {
   return (
@@ -8,6 +9,12 @@ function AddCabin() {
       <Modal.Open opens="create-cabin">
         <Button>Add new cabin</Button>
       </Modal.Open>
+      <Modal.Open opens="cabin-table">
+        <Button>Show cabin table</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-table">
+        <CabinTable />
+      </Modal.Window>
       <Modal.Window name="create-cabin">
         <CreateCabinForm />
       </Modal.Window>
